36abb050e6245fc862554e5f99d2ab4fafacfa2c
eddybob <eddymadu90@gmail.com>
2022-11-11 08:46:29 +0100

    fet:created register and login function

--- src/controllers/auth.controller.ts
+++ src/controllers/auth.controller.ts
@@ -1,6 +1,20 @@
 import successResponse from "../helpers/success.response";
-import { customError } from "../helpers/customError";
-const Auth = {
-  signup: async () => {},
-  signin: async () => {},
-};
+import customError from "../helpers/customError";
+import uploadPhoto from "../utils/uploadPhoto";
+import { Response } from "express";
+import UserEntity from "../model/User";
+
+interface AuthInterface {
+  register: () => Promise<any>;
+  login: () => Promise<any>;
+}
+
+class Auth implements AuthInterface {
+  constructor(private readonly entity: any) {}
+
+  async register(): Promise<any> {}
+
+  async login(): Promise<any> {}
+}
+
+export default new Auth(UserEntity);
